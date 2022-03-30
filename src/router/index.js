35c53c7fae8aaa1aaf05ed4a1d4330e268b8f6d8
liuxueying123 <1390528287@qq.com>
2022-03-30 15:16:34 +0800

    基础大件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    children:[{
+      
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      
+    }]
+  },
+
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <div class="menu">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        background-color="#272727"
+        text-color="#ffffff"
+      >
+        <el-menu-item index="/demo" @click="goTo">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="/index">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+// import router from "../router/index";
+export default {
+  name: "Home",
+  methods: {
+    goTo() {
+      this.$router.push("/about");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  height: 100vh;
+  .menu {
+    width: 15%;
+    .el-menu-vertical-demo {
+      height: 100%;
+    }
+  }
+}
+</style>
